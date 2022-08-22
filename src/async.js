e4be12b9d82fff27bcdfea9d46088cfddb3a5c21
jfr <jonathan.freund@yahoo.com>
2022-08-22 13:20:05 +0200

    Did the course

--- src/async.js
+++ src/async.js
@@ -1,3 +1,24 @@
 import { add } from './helpers';
+import { Observable } from 'rxjs';
 
-add.li('Hello') 
+add.li('Line 4')
+
+async function runPromis() {
+    add.li('Line 7')
+    const p = new Promise(
+        (resolve, reject) =>{
+            setTimeout(
+                () =>{
+                    resolve('We are complete');
+                }, 1000
+            )
+        }
+        
+    );
+    const message = await p;
+    add.li('Line 19')
+}
+
+runPromis()
+
+add.li('Line 21')
